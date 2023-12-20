a02695c0e05432d4cd4719687d4401ae34b26113
AdamJaniczek <152891309+AdamJaniczek@users.noreply.github.com>
2023-12-20 18:17:59 +0100

    Rozwiązanie

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,13 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-
+        Person adam = new Person("Adam", "Nowak");
+        Person tomek = new Person("Tomek", "Malinowski");
+        Task shoping = new Task("Zakupy", "kupić pieczywo", 1, adam);
+        Task cleaning = new Task("Sprzątanie", "Umyć auto w piątek", -1, tomek);
+        Task buyTickets = new Task("Zakupy", "Kupić bilety na koncert", 0, adam);
+        System.out.println(shoping.lowPriority());
+        System.out.println(cleaning.lowPriority());
+        System.out.println(buyTickets.highPriority());
     }
 }
---
+++ src/main/java/pl/javastart/task/Task.java
@@ -0,0 +1,42 @@
+package pl.javastart.task;
+
+public class Task {
+    private String name;
+    private String description;
+    private int priority;
+    private Person responsiblePerson;
+
+    public Task(String name, String description, int priority, Person responsiblePerson) {
+        this.name = name;
+        this.description = description;
+        this.priority = priority;
+        this.responsiblePerson = responsiblePerson;
+    }
+
+    public Task(String name, String description, int priority) {
+        this.name = name;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    boolean highPriority() {
+        return this.priority == 1;
+    }
+
+    boolean mediumPriority() {
+        return this.priority == 0;
+    }
+
+    boolean lowPriority() {
+        return this.priority == -1;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
